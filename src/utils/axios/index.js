46d6227bc62f9e654309c9888ec5e33752f38301
Görkem Şahin <gorkemsahin@Gorkems-MacBook-Pro.local>
2020-10-16 11:06:33 +0300

    Fixed project's ENV config. (.env file is added to the repository since it's a private repo anyway)

--- src/utils/axios/index.js
+++ src/utils/axios/index.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
-import Config from 'react-native-config';
-
-// TODO fix react-native-config and acquire these values from .env file
-const BASE_URL = 'https://api.themoviedb.org/3/'
-const API_KEY = '5c75e7e839cfa630ddd7d8ee2c2d47e6'
+import { BASE_URL, API_KEY } from '@env'
 
 const instance = axios.create({
   baseURL: BASE_URL,
