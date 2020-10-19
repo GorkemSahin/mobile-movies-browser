67d1672698b461f5ab1cd282379c541b557f7c67
Görkem Şahin <gorkemsahin@Gorkems-MacBook-Pro.local>
2020-10-19 18:47:30 +0300

    Added a comment to justify unused dateParser's existence.

--- src/utils/dateParser/index.js
+++ src/utils/dateParser/index.js
@@ -1,3 +1,6 @@
+/* Was used to parse release dates of movies when I first thought I'd display
+dates on cards, but later I replaced them with ratings. */
+
 export default (date) => {
   return new Intl.DateTimeFormat('en-GB').format(new Date(date))
 }
